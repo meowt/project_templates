e359b1f8428544af24aa6b363520a47975ab8581
meowt <matweewilya@gmail.com>
2024-09-08 16:34:57 +0300

    feat(): embedding of api&storage configs into config.Config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,18 @@
 package config
 
+import (
+	"project/internal/api"
+	"project/internal/storage"
+)
+
 const (
 	EnvDev  = "dev"
 	EnvProd = "prod"
 )
 
 type Config struct {
+	Api     api.Config
+	Storage storage.Config
 }
 
 func Load(env string) (*Config, error) {
